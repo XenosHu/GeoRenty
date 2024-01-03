70edcfbf776e805b8f7f3c934b8e8010727165f0
XenosHu <121327330+XenosHu@users.noreply.github.com>
2024-01-02 20:25:16 -0500

    Update geo.py

--- geo.py
+++ geo.py
@@ -7,33 +7,33 @@ import pydeck as pdk
 raw = pd.read_csv('zillow.csv')
 st.write(raw)
 
-# st.pydeck_chart(pdk.Deck(
-#     map_style=None,
-#     initial_view_state=pdk.ViewState(
-#         latitude=-74.1,
-#         longitude=40.7,
-#         zoom=11,
-#         pitch=50,
-#     ),
-#     layers=[
-#         pdk.Layer(
-#            'HexagonLayer',
-#            data=chart_data,
-#            get_position='[lon, lat]',
-#            radius=200,
-#            elevation_scale=4,
-#            elevation_range=[0, 1000],
-#            pickable=True,
-#            extruded=True,
-#         ),
-#         pdk.Layer(
-#             'ScatterplotLayer',
-#             data=raw,
-#             get_position='[longitude, latitude]',
-#             get_color='[200, 30, 0, 160]',
-#             get_radius=200,
-#         ),
-#     ],
-# ))
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=-74.1,
+        longitude=40.7,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=raw,
+            get_position='[longitude, latitude]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
 
 
